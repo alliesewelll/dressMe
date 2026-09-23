40457ea4a66d2fbdcb194b3650ef43e65ac95e4b
Allie <alliesewell@gmail.com>
2026-09-23 15:03:44 -0400

    backend stuff

--- backend/schemas.py
+++ backend/schemas.py
@@ -4,3 +4,12 @@ from datetime import datetime
 class UserCreate(BaseModel):
     name: str
     email: EmailStr
+    
+class UserResponse(BaseModel):
+    user_id: int
+    name: str
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        from_attributes = True   
